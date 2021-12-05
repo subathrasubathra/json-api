6d6ae1bfe97f1ba2275447a082557c9441b9a792
subathrasubathra <subaathraa@gmail.com>
2021-12-05 13:02:57 +0000

    initial commit

---
+++ src/main/java/com/jsonapi/JsonApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsonapi/JsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
